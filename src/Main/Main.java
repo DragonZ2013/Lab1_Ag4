7b1ee62d2a8dd50668681e7857e536171d246d59
DragonZ <drag.popescu@yahoo.com>
2021-10-15 10:38:55 +0300

    Reworked Comments

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,10 +7,10 @@ public class Main {
         int[] KeyboardCost2 = {15,20,10,35};
         int[] KeyboardCost3 = {40,50,60};
         int[] KeyboardCost4 = {60};
-        int[] KeyboardCost5 = {40,60};
+        int[] KeyboardCost5 = {55,60,45};
         int[] USBCost2 = {20,15,40,15};
         int[] USBCost3 = {15,45,20};
-        int[] USBCost4 = {8,12};
+        int[] USBCost4 = {10,12};
         System.out.println(aufgabe_1(KeyboardCost1));
         System.out.println(aufgabe_2(KeyboardCost2,USBCost2));
         System.out.println(aufgabe_3(USBCost3,30));
@@ -19,10 +19,12 @@ public class Main {
         System.out.println(aufgabe_4(KeyboardCost5,USBCost4,60));
     }
 
+    /**
+     * returns the lowest cost from the array
+     * @param costKeyboard:int[]
+     * @return int
+     */
     public static int aufgabe_1(int[] costKeyboard){
-        //returns the lowest cost from the array
-        //Input: int[]
-        //Output: int
         int min_cost=costKeyboard[0];
         for(int cost: costKeyboard)
             if(min_cost>cost)
@@ -30,10 +32,13 @@ public class Main {
         return min_cost;
     }
 
+    /**
+     * returns the highest cost from both arrays
+     * @param costKeyboard:int[]
+     * @param costUSB:int[]
+     * @return int
+     */
     public static int aufgabe_2(int[] costKeyboard,int[] costUSB){
-        //returns the highest cost from both arrays
-        //Input: int[],int[]
-        //Output: int
         int max_cost=costKeyboard[0];
         for(int cost: costKeyboard)
             if(max_cost<cost)
@@ -44,10 +49,13 @@ public class Main {
         return max_cost;
     }
 
+    /**
+     *returns the highest cost from array under budget
+     * @param costUSB:int[]
+     * @param budget:int
+     * @return int
+     */
     public static int aufgabe_3(int[] costUSB,int budget){
-        //returns the highest cost from array under budget
-        //Input: int[],int
-        //Output: int
         int max_cost=costUSB[0];
         for(int cost: costUSB)
             if(max_cost<cost&&cost<budget)
@@ -55,10 +63,14 @@ public class Main {
         return max_cost;
     }
 
+    /**
+     * returns the highest cost of 2 added elements from each array, under budget
+     * @param costKeyboard:int[]
+     * @param costUSB:int[]
+     * @param budget:int[]
+     * @return int
+     */
     public static int aufgabe_4(int[] costKeyboard,int[] costUSB,int budget){
-        //returns the highest cost of 2 added elements from each array, under budget
-        //Input: int[],int[],int
-        //Output: int
         int max_cost=-1;
         for(int cost1: costKeyboard)
             for(int cost2: costUSB)
